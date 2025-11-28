2ed21ba4b420fa348a41ca3a30c8b9b1ed2031b5
zahra-22 <zahraaden22@hotmail.com>
2025-11-28 16:54:06 -0500

    fix CRA env variable for deployment

--- my-portfolio/src/api.js
+++ my-portfolio/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE_URL = process.env.REACT_APP_API || "http://localhost:5000";
 
 export const apiRequest = async (url, method = "GET", body) => {
   const res = await fetch(BASE_URL + url, {
